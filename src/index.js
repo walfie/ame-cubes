c06ff337e462f750d7f640c75e2a305ff6ad6457
Walfie <walfington@gmail.com>
2020-11-07 18:26:51 -0500

    Add cubes and lighting

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ const spriteData = {
   image: spritesheet,
   frameCount: 8,
   fps: 12,
+  rotationRate: 0.05,
 };
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  60,
+  75, // FOV
   window.innerWidth / window.innerHeight,
-  1,
-  2100
+  0.1, // near
+  3000 // far
 );
 camera.position.z = 1000;
 
@@ -41,12 +42,12 @@ document.body.appendChild(renderer.domElement);
 window.addEventListener("resize", onWindowResize, false);
 
 class AnimatedSprite {
-  constructor({ width, height, spritesheet, frameCount, fps }) {
+  constructor({ width, height, spritesheet, frameCount, fps, rotationRate }) {
     this.width = width;
     this.height = height;
     this.frameCount = frameCount;
     this.setFrameRate(fps);
-    this.setRotationRate(-0.2);
+    this.setRotationRate(rotationRate);
 
     this.texture = new THREE.TextureLoader().load(spriteData.image);
     this.material = new THREE.SpriteMaterial({
@@ -76,10 +77,10 @@ class AnimatedSprite {
   }
 
   update(delta) {
+    this.material.rotation += this.rotationRate;
     this.elapsed += delta;
 
     while (this.elapsed > this.frameDelay) {
-      this.material.rotation += this.rotationRate;
       this.elapsed -= this.frameDelay;
       this.currentFrame = (this.currentFrame + 1) % this.frameCount;
       this.texture.offset.x = this.currentFrame / this.frameCount;
@@ -90,19 +91,122 @@ class AnimatedSprite {
 const sprite = new AnimatedSprite(spriteData);
 scene.add(sprite.sprite);
 
-const gui = new dat.GUI();
+let choice = (a, b) => {
+  return Math.random() > 0.5 ? a : b;
+};
+
+class Cube {
+  constructor() {
+    const geometry = new THREE.BoxBufferGeometry(100, 100, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff00ff });
+
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.velocity = { x: 0, y: 0, z: 0 };
+    this.randomize();
+  }
+
+  randomize() {
+    this.rotationRate = {
+      x: (Math.random() - 0.5) * 0.1,
+      y: (Math.random() - 0.5) * 0.1,
+      z: (Math.random() - 0.5) * 0.1,
+    };
+
+    const scale = 1 + Math.random() * 3;
+    this.mesh.scale.x = scale;
+    this.mesh.scale.y = scale;
+    this.mesh.scale.z = scale;
+    this.mesh.material.color.set(Math.random() * 16777215); // TODO
+
+    const max = 3000;
+    const [edgeAxis, otherAxis] = choice(["x", "y"], ["y", "x"]);
+
+    const isMinEdge = choice(true, false);
+
+    this.mesh.position[edgeAxis] = isMinEdge ? -max : max;
+    this.mesh.position[otherAxis] = (Math.random() - 0.5) * 2 * max;
+    this.mesh.position.z = -Math.random() * 1000;
+
+    this.velocity.z = Math.random() - 0.5;
+    this.velocity[edgeAxis] = Math.random() * 50 * (isMinEdge ? 1 : -1);
+    this.velocity[otherAxis] = (Math.random() - 0.5) * 100;
+  }
+
+  update() {
+    this.mesh.rotation.x += this.rotationRate.x;
+    this.mesh.rotation.y += this.rotationRate.y;
+    this.mesh.rotation.z += this.rotationRate.z;
+
+    this.mesh.position.x += this.velocity.x;
+    this.mesh.position.y += this.velocity.y;
+    this.mesh.position.z += this.velocity.z;
+
+    const pos = this.mesh.position;
+    const max = 5000;
+    if (
+      pos.x < -max ||
+      pos.x > max ||
+      pos.y < -max ||
+      pos.x > max ||
+      pos.z < -max ||
+      pos.z > max
+    ) {
+      this.randomize();
+    }
+  }
+}
+
+const cubes = [...Array(100)].map((_) => {
+  const cube = new Cube();
+  scene.add(cube.mesh);
+  return cube;
+});
+
 const params = {
   rotationRate: sprite.rotationRate,
   spriteFps: spriteData.fps,
+  backgroundColor: "#ffffff",
+  lightColor: "#ffffff",
+  lightIntensity: 1.0,
 };
-gui
-  .add(params, "rotationRate", -1, 1)
+
+const light = new THREE.PointLight(params.lightColor, 1.0, 0);
+light.position.z = camera.position.z;
+scene.add(light);
+
+const gui = new dat.GUI();
+const sceneControls = gui.addFolder("Scene");
+sceneControls.open();
+sceneControls
+  .addColor(params, "backgroundColor")
+  .name("Background")
+  .onChange(() => {
+    renderer.setClearColor(params.backgroundColor);
+  });
+sceneControls
+  .addColor(params, "lightColor")
+  .name("Lighting")
+  .onChange(() => {
+    light.color.set(params.lightColor);
+  });
+sceneControls
+  .add(params, "lightIntensity", 0.0, 5.0)
+  .name("Light Intensity")
+  .onChange(() => {
+    light.intensity = params.lightIntensity;
+  });
+
+const ameControls = gui.addFolder("Ame");
+ameControls.open();
+
+ameControls
+  .add(params, "rotationRate", -0.3, 0.3)
   .name("Rotation rate")
   .onChange(() => {
     sprite.setRotationRate(params.rotationRate);
   });
 
-gui
+ameControls
   .add(params, "spriteFps", 1, 60)
   .name("Sprite FPS")
   .onChange(() => {
@@ -119,6 +223,7 @@ const animate = () => {
   requestAnimationFrame(animate);
   const delta = clock.getDelta();
   sprite.update(1000 * delta);
+  cubes.forEach((cube) => cube.update());
 
   renderer.clear();
   renderer.render(scene, camera);
